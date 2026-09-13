0964efca65a192bfb788c1c299376040201f3eab
Khragg <qwertyuiopqaz_0930@qq.com>
2026-09-13 19:39:26 +0800

    chore: 移除仓库内诊断脚本与临时日志等垃圾文件

--- checklog.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-import io
-import sys
-sys.stdout.reconfigure(encoding="utf-8")
-t = io.open(r"C:\Users\Sketch\.zcode\workspace\default\promaid-mod\compile_out.txt",
-            encoding="utf-8", errors="replace").read().splitlines()
-for l in t:
-    print(ascii(l[:160]))
